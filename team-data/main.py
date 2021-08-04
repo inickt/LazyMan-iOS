78e788458376216c9f3ed886d80ce6db0dace301
Nick Thompson <git@nickt.dev>
2021-08-03 23:16:53 -0400

    Update MLB team data, fix deployment target

--- team-data/main.py
+++ team-data/main.py
@@ -1,80 +1,115 @@
 import requests
 
+from enum import Enum, auto
 import json
 from pathlib import Path
 import plistlib
 
 BASE_PATH = Path("build")
+LOGO_XCASSET_PATH = BASE_PATH / "Logos.xcassets"
+ASSET_INFO = {"info": {"version": 1, "author": "xcode"}}
 
 
-def nhl_team_data():
+class League(Enum):
+    NHL = auto()
+    MLB = auto()
 
-    LIGHT_SVG_URL = "https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/{}.svg"
-    DARK_SVG_URL = "https://www-league.nhlstatic.com/images/logos/teams-current-primary-dark/{}.svg"
+    @property
+    def teams_url(self):
+        return {
+            League.NHL: "https://statsapi.web.nhl.com/api/v1/teams?fields=abbreviation,teams,teamName,id,name",
+            League.MLB: "https://statsapi.mlb.com/api/v1/teams?fields=abbreviation,teams,teamName,id,name&sportIds=1",
+        }[self]
 
-    response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
-    json_teams = response.json()["teams"]
+    def logo_variants(self, id):
+        return [("light", self.light_logo_url(id)), ("dark", self.dark_logo_url(id))]
 
-    teams = []
-    for team in json_teams:
-        name = team["name"]
-        team_name = team["teamName"]
-        abbreviation = team["abbreviation"]
-        id = team["id"]
-        teams.append(
+    def light_logo_url(self, id):
+        return {
+            League.NHL: f"https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/{id}.svg",
+            League.MLB: f"https://www.mlbstatic.com/team-logos/team-cap-on-light/{id}.svg",
+        }[self]
+
+    def dark_logo_url(self, id):
+        return {
+            League.NHL: f"https://www-league.nhlstatic.com/images/logos/teams-current-primary-dark/{id}.svg",
+            League.MLB: f"https://www.mlbstatic.com/team-logos/team-cap-on-dark/{id}.svg",
+        }[self]
+
+
+def team_data(league: League):
+    print(f"Generating {league.name} team logos and name data")
+    teams_response = requests.get(league.teams_url)
+    teams = sorted(teams_response.json()["teams"], key=lambda t: t["name"])
+
+    for team in teams:
+        print(f" {team['name']} ({team['abbreviation']})")
+        make_imageset(league, team)
+        del team["id"]
+        team["league"] = league.name
+
+    write_asset_contents(
+        LOGO_XCASSET_PATH / league.name,
+        {
+            **ASSET_INFO,
+            "properties": {"provides-namespace": True},
+        },
+    )
+
+    make_plist(league, teams)
+    print(f"Finished generating {league.name} team logos and name data\n")
+
+
+def make_imageset(league: League, team):
+    team_name, id = team["teamName"], team["id"]
+    filename_prefix = team_name.lower().replace(" ", "-")
+    imageset_path = LOGO_XCASSET_PATH / league.name / (filename_prefix + ".imageset")
+    imageset_path.mkdir(parents=True, exist_ok=True)
+
+    images = []
+    for kind, url in league.logo_variants(id):
+        image_path = imageset_path / (filename_prefix + "-" + kind + ".svg")
+        print(f"  Writing {team_name} {kind} logo to {image_path}")
+        image_response = requests.get(url)
+        with open(image_path, "wb") as file:
+            file.write(image_response.content)
+        images.append(
             {
-                "name": name,
-                "teamName": team_name,
-                "abbreviation": abbreviation,
-                "league": "NHL",
+                "idiom": "universal",
+                "filename": image_path.name,
+                "appearances": [{"appearance": "luminosity", "value": kind}],
             }
         )
 
-        filename_prefix = team_name.lower().replace(" ", "-")
-        print(f"{name=}\n{team_name=}\n{abbreviation=}\n{filename_prefix=}")
+    write_asset_contents(
+        imageset_path,
+        {
+            "images": images,
+            **ASSET_INFO,
+            "properties": {"preserves-vector-representation": True},
+        },
+    )
 
-        nhl_path = BASE_PATH / "NHL"
-        imageset_path = nhl_path / (filename_prefix + ".imageset")
-        imageset_path.mkdir(parents=True, exist_ok=True)
-        for kind, url in [("light", LIGHT_SVG_URL), ("dark", DARK_SVG_URL)]:
-            image_path = imageset_path / (filename_prefix + "-" + kind + ".svg")
-            print(f"{image_path}")
 
-            image = requests.get(url.format(id))
-            with open(image_path, "wb") as file:
-                file.write(image.content)
-        contents_path = imageset_path / "Contents.json"
-        with open(contents_path, "w") as file:
-            json.dump(
-                {
-                    "images": [
-                        {
-                            "filename": filename_prefix + "-light.svg",
-                            "idiom": "universal",
-                        },
-                        {
-                            "appearances": [
-                                {"appearance": "luminosity", "value": "dark"}
-                            ],
-                            "filename": filename_prefix + "-dark.svg",
-                            "idiom": "universal",
-                        },
-                    ],
-                    "info": {"author": "xcode", "version": 1},
-                    "properties": {"preserves-vector-representation": True},
-                },
-                file,
-                indent=2,
-            )
-
-        print()
-
-    teams.sort(key=lambda t: t["name"])
-    nhl_plist_path = BASE_PATH / "NHLTeams.plist"
-    print(f"Writing NHL team data ({len(teams)}) to {nhl_plist_path}")
-    with open(nhl_plist_path, "wb") as file:
+def make_plist(league: League, teams):
+    plist_path = BASE_PATH / (league.name + "Teams.plist")
+    print(f"Writing {league.name} team data ({len(teams)}) to {plist_path}")
+    with open(plist_path, "wb") as file:
         plistlib.dump(teams, file)
 
 
+def write_asset_contents(path, attributes):
+    with open(path / "Contents.json", "w") as file:
+        json.dump(
+            attributes,
+            file,
+            indent=2,
+            separators=(",", " : "),
+        )
+
+
 if __name__ == "__main__":
-    nhl_team_data()
+    for league in League:
+        team_data(league)
+
+    write_asset_contents(LOGO_XCASSET_PATH, ASSET_INFO)
